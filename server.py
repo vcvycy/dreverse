621c27916e452e1bc369fb33cdeb4883067688e7
vcvycy <352871242@qq.com>
2018-06-09 15:04:19 +0800

    0.0.0.0

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket                                                                   
-addr=("127.0.0.1",8080)
+addr=("0.0.0.0",8080)
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind(addr)
 s.listen(1)
@@ -12,12 +12,5 @@ while True:
   conn,addr=s.accept()
   print("[*]New")
   print(conn.recv(1024))
-  conn.send(resp)
-  """
-  conn.send(b"HTTP/1.1 200 OK\r\n\
-              Content-Type: text/html\r\n\
-              Content-Length: 5\r\n\
-	    	 \r\n\
-		     hello") 
-			 """
+  conn.send(resp) 
   conn.close()
